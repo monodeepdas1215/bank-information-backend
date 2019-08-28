927d3c28be05fe7470fe35ff94dc3f14788daff5
Monodeep Das <monodeepdas1215@gmail.com>
2019-08-29 04:30:32 +0530

    heroku deployment changes

--- src/rest/banks_api.py
+++ src/rest/banks_api.py
@@ -7,6 +7,14 @@ from src.services.db_services import get_paginated_results, get_bank_details_ser
 api = Blueprint('banks_api', __name__, url_prefix='/api')
 
 
+@api.route('/', methods=["GET"])
+def get_all_available_apis():
+    return jsonify({
+        '/api/banks/<ifsc>': 'search bank branches by ifsc code',
+        '/api/banks/<bank_name>/branches/<city>': 'search bank branches by name of bank and city'
+    }), 200
+
+
 @api.route('/banks/<ifsc>', methods=['GET'])
 @authenticated_access
 def get_banks(ifsc):
@@ -35,7 +43,7 @@ def get_branches_details(bank_name, city):
     try:
         # reading the request arguments
         offset = request.args.get('offset', 0)
-        limit = request.args.get('limit', 2)
+        limit = request.args.get('limit', 10)
 
         # get the entire results
         branches = get_all_branches_details_service(bank_name, city)
